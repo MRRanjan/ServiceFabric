96377cbf2559657cf23e57a7ae6c3d687cd9726d
MRRanjan <devrajranjan@outlook.com>
2017-05-18 00:01:20 -0400

    AlphabetPartitions

---
+++ ReliableServices/AlphabetPartitions/Alphabet.StatelessSrv/StatelessSrv.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Diagnostics;
+using System.Fabric.Description;
+using System.Net;
+using System.Text;
+
+namespace Alphabet.StatelessSrv
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class StatelessSrv : StatelessService
+    {
+        string instanceId = "";
+        int instanceCount = 0;
+        long iterations = 0;
+
+        public StatelessSrv(StatelessServiceContext context)
+            : base(context)
+        {
+            instanceId = $"{Process.GetCurrentProcess().Id}#{context.InstanceId}";
+            ServiceEventSource.Current.Message($"Alphabet.StatelessSrv.StatelessSrv.Ctor: {instanceId} / {++instanceCount}");
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            ServiceEventSource.Current.Message($"Alphabet.StatelessSrv.StatelessSrv.CreateServiceReplicaListeners: {instanceId} / {++instanceCount}");
+            return new[] { new ServiceInstanceListener(context => this.CreateInternalListener(context)) };
+            //return new ServiceInstanceListener[0];
+        }
+
+        private ICommunicationListener CreateInternalListener(ServiceContext context)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.StatelessSrv.StatelessSrv.CreateInternalListener: {instanceId} / {++instanceCount}");
+            
+            EndpointResourceDescription internalEndpoint = context.CodePackageActivationContext.GetEndpoint("StatelessSrvEndpoint");
+
+            string uriPrefix = String.Format(
+                "{0}://+:{1}/{2}/{3}/",
+                internalEndpoint.Protocol,
+                internalEndpoint.Port,
+                context.PartitionId,
+                context.ReplicaOrInstanceId);
+
+            string nodeIP = FabricRuntime.GetNodeContext().IPAddressOrFQDN;
+
+            // The published URL is slightly different from the listening URL prefix.
+            // The listening URL is given to HttpListener.
+            // The published URL is the URL that is published to the Service Fabric Naming Service,
+            // which is used for service discovery. Clients will ask for this address through that discovery service.
+            // The address that clients get needs to have the actual IP or FQDN of the node in order to connect,
+            // so we need to replace '+' with the node's IP or FQDN.
+            string uriPublished = uriPrefix.Replace("+", nodeIP);
+            return new HttpCommunicationListener(uriPrefix, uriPublished, this.ProcessInternalRequest);
+        }
+
+        private async Task ProcessInternalRequest(HttpListenerContext context, CancellationToken cancelRequest)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.StatelessSrv.StatelessSrv.ProcessInternalRequest: {instanceId} / {++instanceCount}");
+            string output = null;
+            string user = context.Request.QueryString["lastname"].ToString();
+
+            try
+            {
+                output = $"<h4>StatelessSrv.ProcessInternalRequest:</h4> {instanceId} / {instanceCount}<br>";
+                output = output + $"Response ({iterations}) > {Reverse(user)}";
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+            }
+
+            using (HttpListenerResponse response = context.Response)
+            {
+                if (output != null)
+                {
+                    byte[] outBytes = Encoding.UTF8.GetBytes(output);
+                    response.OutputStream.Write(outBytes, 0, outBytes.Length);
+                }
+            }
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                ++iterations;               
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
---
+++ ReliableServices/AlphabetPartitions/Alphabet.Web/Web.cs
@@ -0,0 +1,198 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Alphabet.WebApi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Fabric;
+    using System.Fabric.Description;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Services.Client;
+    using Microsoft.ServiceFabric.Services.Communication.Runtime;
+    using Microsoft.ServiceFabric.Services.Runtime;
+    using Newtonsoft.Json.Linq;
+    using System.Diagnostics;
+
+    public class Web : StatelessService
+    {
+        private static readonly Uri alphabetProcessingUri = new Uri(@"fabric:/AlphabetPartitions/Processing");
+        private static readonly Uri alphabetStatelessSrvUri = new Uri(@"fabric:/AlphabetPartitions/StatelessSrv");
+
+        private readonly ServicePartitionResolver serviceProcessingResolver = ServicePartitionResolver.GetDefault();
+        private readonly ServicePartitionResolver serviceStatelessSrvResolver = ServicePartitionResolver.GetDefault();
+
+        private readonly HttpClient httpProcessingClient = new HttpClient();
+        private readonly HttpClient httpStatelessSrvClient = new HttpClient();
+
+        string instanceId = "";
+        int instanceCount = 0;
+
+        public Web(StatelessServiceContext context): base(context)
+        {
+            instanceId = $"{Process.GetCurrentProcess().Id}#{context.InstanceId}";
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.Ctor: {instanceId} / {++instanceCount}");
+        }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.CreateServiceInstanceListeners: {instanceId} / {++instanceCount}");
+            return new[] {new ServiceInstanceListener(context => this.CreateInputListener(context))};
+        }
+
+        private ICommunicationListener CreateInputListener(ServiceContext context)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.CreateInputListener: {instanceId} / {++instanceCount}");
+            // Service instance's URL is the node's IP & desired port
+            EndpointResourceDescription inputEndpoint = context.CodePackageActivationContext.GetEndpoint("WebApiServiceEndpoint");
+
+            // This is the public-facing URL that HTTP clients, e.g., web browsers, can connect to.
+            // The "alphabetpartitions" path is a unique URL prefix for this service so that other
+            // services that might be hosted on the same node can also use this port with their own unique URL prefix.
+            string uriPrefix = String.Format("{0}://+:{1}/alphabetpartitions/", inputEndpoint.Protocol, inputEndpoint.Port);
+            
+            // The published URL is slightly different from the listening URL prefix.
+            // The listening URL is given to HttpListener.
+            // The published URL is the URL that is published to the Service Fabric Naming Service,
+            // which is used for service discovery. Clients will ask for this address through that discovery service.
+            // The address that clients get needs to have the actual IP or FQDN of the node in order to connect,
+            // so we need to replace '+' with the node's IP or FQDN.
+            string uriPublished = uriPrefix.Replace("+", FabricRuntime.GetNodeContext().IPAddressOrFQDN);
+
+            return new HttpCommunicationListener(uriPrefix, uriPublished, this.ProcessInputRequest);
+        }
+
+        private async Task ProcessInputRequest(HttpListenerContext context, CancellationToken cancelRequest)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.ProcessInputRequest: {instanceId} / {++instanceCount}");
+
+            string lastname = context.Request.QueryString["lastname"];
+
+            StringBuilder output = new StringBuilder();
+            output.AppendFormat($"<h3>Web.ProcessInputRequest:</h3> {instanceId} / {instanceCount}<br>Processing lastname={lastname}<br><br>");
+
+            var tasks = new List<Task<string>>();
+            tasks.Add(ProcessStateful(lastname, cancelRequest));
+            tasks.Add(ProcessStateless(lastname, cancelRequest));
+            await Task.WhenAll(tasks);
+            
+            foreach (Task<string> task in tasks)
+            {
+                try
+                {
+                    output.AppendFormat($"{task.Result}<br><br>");
+                }
+                catch (Exception ex)
+                {
+                    output.AppendFormat("<h3>Exception:</h3>{0}<br><br>", ex.Message);
+                }
+            }
+
+            using (HttpListenerResponse response = context.Response)
+            {
+                response.ContentType = "text/html";
+
+                byte[] outBytes = Encoding.UTF8.GetBytes(output.ToString());
+                response.OutputStream.Write(outBytes, 0, outBytes.Length);
+            }
+        }
+
+        private async Task<string> ProcessStateful(string lastname, CancellationToken cancelRequest)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.ProcessStateful: {instanceId} / {++instanceCount}");
+            String output = null;
+
+            try
+            {
+
+                // The partitioning scheme of the processing service is a range of integers from 0 - 25.
+                // This generates a partition key within that range by converting the first letter of the input name
+                // into its numerica position in the alphabet.
+                char firstLetterOfLastName = lastname.First();
+                long partitionId = (Char.ToUpper(firstLetterOfLastName) - 'A') / 9;
+
+                ServicePartitionKey partitionKey = new ServicePartitionKey(partitionId);
+
+                // This contacts the Service Fabric Naming Services to get the addresses of the replicas of the processing service 
+                // for the partition with the partition key generated above. 
+                // Note that this gets the most current addresses of the partition's replicas,
+                // however it is possible that the replicas have moved between the time this call is made and the time that the address is actually used
+                // a few lines below.
+                // For a complete solution, a retry mechanism is required.
+                // For more information, see http://aka.ms/servicefabricservicecommunication
+                ResolvedServicePartition partition = await this.serviceProcessingResolver.ResolveAsync(alphabetProcessingUri, partitionKey, cancelRequest);
+                ResolvedServiceEndpoint ep = partition.GetEndpoint();
+
+                JObject addresses = JObject.Parse(ep.Address);
+                string primaryReplicaAddress = (string)addresses["Endpoints"].First();
+
+                UriBuilder primaryReplicaUriBuilder = new UriBuilder(primaryReplicaAddress);
+                primaryReplicaUriBuilder.Query = "lastname=" + lastname;
+
+                string result = await this.httpProcessingClient.GetStringAsync(primaryReplicaUriBuilder.Uri);
+
+                output = String.Format(
+                    "<h3>Stateful Result:</h3> {0}.<p>Partition key: '{1}' generated from the first letter '{2}' of input value '{3}'. <br>Processing service partition ID: {4}. <br>Processing service replica address: {5}",
+                    result,
+                    partitionKey,
+                    firstLetterOfLastName,
+                    lastname,
+                    partition.Info.Id,
+                    primaryReplicaAddress);
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+            }
+
+            return output;
+        }
+
+        private async Task<string> ProcessStateless(string lastname, CancellationToken cancelRequest)
+        {
+            ServiceEventSource.Current.Message($"Alphabet.WebApi.Web.ProcessStateless: {instanceId} / {++instanceCount}");
+            String output = null;
+
+            try
+            {
+                // This contacts the Service Fabric Naming Services to get the addresses of the replicas of the processing service 
+                // for the partition with the partition key generated above. 
+                // Note that this gets the most current addresses of the partition's replicas,
+                // however it is possible that the replicas have moved between the time this call is made and the time that the address is actually used
+                // a few lines below.
+                // For a complete solution, a retry mechanism is required.
+                // For more information, see http://aka.ms/servicefabricservicecommunication
+                ResolvedServicePartition partition = await this.serviceStatelessSrvResolver.ResolveAsync(alphabetStatelessSrvUri, new ServicePartitionKey(), cancelRequest);
+                ResolvedServiceEndpoint ep = partition.GetEndpoint();
+
+                JObject addresses = JObject.Parse(ep.Address);
+                string primaryReplicaAddress = (string)addresses["Endpoints"].First();
+
+                UriBuilder primaryReplicaUriBuilder = new UriBuilder(primaryReplicaAddress);
+                primaryReplicaUriBuilder.Query = "lastname=" + lastname;
+
+                string result = await this.httpProcessingClient.GetStringAsync(primaryReplicaUriBuilder.Uri);
+
+                output = String.Format(
+                    "<h3>Stateless Result:</h3> {0}.<p>Generated from the input value '{1}'. <br>Processing service partition ID: {2}. <br>Processing service replica address: {3}",
+                    result,
+                    lastname,
+                    partition.Info.Id,
+                    primaryReplicaAddress);
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+            }
+
+            return output;
+        }
+    }
+}
